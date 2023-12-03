76efb5c436242f667e3f05ba62b3d162ef4a6ec0
Elshod Musurmonov <elshodatc1116@gmail.com>
2023-12-03 17:51:09 +0500

    Controllerlar

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,49 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ShowProfileController;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+//Controllerlar
+// php artisan make:controller UserController  -> Controoller xosil qilish
+Route::get('/user/{id?}',[UserController::class,'show']);
 
+// Faqat bir vazifani bajaruvchi controllerlar
+// php artisan make:controller ShowProfileController --invokable
+Route::get('/show/{id}',ShowProfileController::class);
+
+
+
+/*
 Route::get('/hello/{name}', function($name){
     return "Hello Worlid ".$name;
 });
-
-Route::get('/hello/{name?}', function($name=null){
-    return "Hello Worlid";
+// Malum o'zgazivchilarni larni olish
+Route::get('/search', function(Request $request){
+    $name = $request->get('name','Elshod');
+    return $name;
 });
+// Barcha Get o'zgarivchilarni olish
+Route::get('/search1', function(Request $request){
+    $name = $request->all();
+    return $name;
+});
+// Routlarni guruhlash
+Route::group(['prefix'=>'dashboard'],function(){
+    Route::get("users", function(){
+        return 'User Dashboard';
+    });
+    Route::get("techer", function(){
+        return 'Techer Dashboard';
+    });
+    Route::get("statistik", function(){
+        return 'statistik Dashboard';
+    });
+});
+*/
+
